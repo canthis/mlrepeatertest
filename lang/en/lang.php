279b48c513691da85321c3333945d87f12091f6c
BFR.DnB <slavix.dnb@gmail.com>
2017-02-08 18:08:26 +0200

    EN translation

---
+++ lang/en/lang.php
@@ -0,0 +1,7 @@
+<?php return [
+    'plugin' => [
+        'name' => 'MLRepeaterTest',
+        'description' => 'This plugin demonstrates issue #218 of Translate Plugin
+https://github.com/rainlab/translate-plugin/issues/218'
+    ]
+];
